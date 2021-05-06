c4039d2f26a8429225b121f298aa09f67cf5d654
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-05-06 17:17:02 +0200

    Initial Setting - creation of a basic UI -adding a basic css -adding the basic js logic

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+var openMenu = () =>{
+    var btn_start = document.querySelector(".ace");
+    var form = document.querySelector(".form");
+    var table = document.querySelector(".table");
+    btn_start.remove();
+    form.style.display = "flex";
+}
