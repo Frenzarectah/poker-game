b5f79448c85654f50ff5ef81fe7568e0307dd488
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-05-11 17:30:39 +0200

    changing name to some card images

---
+++ js/cards.js
@@ -0,0 +1,10 @@
+class assopicca{
+    constructor(value,seed,src){
+        this.value = 1;
+        this.seme = "picche";
+        this.src = "1C.jpg";
+    }
+
+}
+module.exports={assopicca};
+
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var cards = ["1C.jpg","1D.jpg","1H.jpg","1S.jpg","2C.jpg","2D.jpg","2H.jpg","2S.jpg"];
+
 
 var openMenu = () =>{
     var btn_start = document.querySelector(".ace");
@@ -14,12 +14,21 @@ var sendData = () =>{
     return data;
 }
 /**funzione atta al controllo dell'immissione del nome utente che non può essere vuoto */
-var checkData = (data) =>{
-    if ((data[0] === "null")||(data[0] === "undefined")||(data[0] ==="")) return false;
-    else return data;
+var checkData = (data,cards) =>{
+        if ((data[0] === "null")||(data[0] === "undefined")||(data[0] ==="")) return false;
+        else{ 
+            giveCards(data);
+            return data;
+        }
+}
+var giveCards = (data) =>{
+    const cards = require ("./cards");
+    /*var cards = ["1C.jpg","1D.jpg","1H.jpg","1S.jpg","2C.jpg","2D.jpg","2H.jpg","2S.jpg"];*/
+        for (i=0;i<=data[1]-1;i++){
+            console.log(cards[i]);
+            console.log(cards.assopicca);
+        }
 }
 
 
-
 module.exports = {checkData};
-
